b4ae2c454fd47a6d516ce6699ae1ac548157a1fe
Felipe Contreras <felipe.contreras@gmail.com>
2022-01-17 09:33:56 -0600

    Add water evaluation test
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- tests/lib.js
+++ tests/lib.js
@@ -51,4 +51,24 @@ test('exponential', () => {
   close(brent(x => (x + 3) * Math.pow(x - 1, 2), -4, 0, 1e-7, 1e-10, 50), -3);
 });
 
+test('water evaluate', () => {
+  function water_evaluate(t) {
+    const T_r = 647.096;
+    const na = [ -9.756396, 3.335760, -1.100292, 0.020376, -2.666858, 6.676721 ];
+    const ta = [ 1.018, 1.206, 2.327, 5.753, 4.215, 14.951 ];
+    const reducing_value = 22064000.0;
+
+    const theta = 1 - t / T_r;
+    let sum = 0;
+
+    for (const [i, n] of na.entries())
+      sum += n * theta ** ta[i];
+
+    return reducing_value * Math.exp(T_r / t * sum);
+  }
+
+  const p = 101325;
+  close(brent(t => water_evaluate(t) - p, 273, 647, 1e-7, 1e-10, 50), 373.1255);
+});
+
 test.run();
